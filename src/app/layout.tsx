e4e5f205a488f89d5a9f675741d9ca5f26bf009d
Serhii Borysenko <borisenko37@gmail.com>
2025-02-15 12:21:11 +0100

    add title and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+        <title>Welcome to CRM</title>
+      </head>
       <body className={font.className}>
         <Providers>{children}</Providers>
       </body>
